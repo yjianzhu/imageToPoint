825628fa705938ebcd3ae52b86f2f19638e02547
zhu yongjian <yjianzhu@mail.ustc.edu.cn>
2021-12-17 15:17:19 +0800

    a

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,28 @@
 #include<fstream>
 #include<map>
 
+using namespace std;
 
 
 
-int main()
+int main(int argc,char *argv[])
 {
+    fstream read;
+    read.open(argv[1],ios::in);
+
+    read.seekg(0,read.end);
+    int file_length=read.tellg();
+    read.seekg(0,read.beg);
+
+    char* buffer=new char[file_length];
+    read.read(buffer,file_length);
+
+    read.close();
+
+    cout<<buffer<<endl;
+
+    delete[] buffer;  
     
+
+    return 0;
 }
