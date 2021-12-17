82345c27dcff59319c0af2cf99f5bf5a4105bb69
yjianzhu <yjianzhu@mail.ustc.edu.cn>
2021-12-17 22:34:19 +0800

    add

--- src/image.h
+++ src/image.h
@@ -2,5 +2,18 @@
 #define IMAGE
 #include "color.h"
 
+class image
+{
+private:
+    int width,
+        height,
+        frames;
+    short *delays;
+    /* data */
+public:
+    image(const char * file/* args */);
+    ~image();
+};
+
 
 #endif
